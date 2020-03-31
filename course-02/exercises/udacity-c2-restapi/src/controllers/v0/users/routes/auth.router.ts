6a3732d23b1260e2fd95ae731890f8afc3b209df
Hansy Schmitt <github@hansyschmitt.com>
2020-03-30 20:51:59 -0500

    fix: Fix generateJWT function to use user json instead of user object

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -22,23 +22,25 @@ async function comparePasswords(plainTextPassword: string, hash: string): Promis
 }
 
 function generateJWT(user: User): string {
-    return jwt.sign(user, config.jwt.secret);
+    return jwt.sign(user.toJSON(), config.jwt.secret);
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
      if (!req.headers || !req.headers.authorization){
          return res.status(401).send({ message: 'No authorization headers.' });
      }
-    
+     
 
      const token_bearer = req.headers.authorization.split(' ');
+     console.log(token_bearer);
+
      if(token_bearer.length != 2){
          return res.status(401).send({ message: 'Malformed token.' });
      }
     
      const token = token_bearer[1];
 
-     return jwt.verify(token, "hello", (err, decoded) => {
+     return jwt.verify(token, config.jwt.secret, (err, decoded) => {
        if (err) {
          return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
        }
